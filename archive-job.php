dfb4018caf475b77ea5e90941a48e944a52a24b5
Patrick Appelman <patrick@appelmandesigns.com>
2025-07-01 22:54:31 -0500

    fix: job filters and pagination

--- archive-job.php
+++ archive-job.php
@@ -60,7 +60,10 @@ $search_query = isset($_GET['search']) ? sanitize_text_field($_GET['search']) : 
 $status_query = isset($_GET['job_opening_status']) ? sanitize_text_field($_GET['job_opening_status']) : 'in-progress';
 $paged = (get_query_var('page')) ? get_query_var('page') : 1;
 
+$status_all = "";
+
 if ($status_query == 'all') {
+	$status_all = $status_query;
 	$status_query = explode(',', 'in-progress,filled,cancelled,declined,inactive,submitted-by-client');
 }
 
@@ -191,7 +194,8 @@ $arr_job_status = array(
 						"optionTemplate": "<div class=\"flex items-center\"><div class=\"size-8 me-2\" data-icon></div><div><div class=\"text-sm font-semibold text-gray-800 dark:text-neutral-200 \" data-title></div><div class=\"text-xs text-gray-500 dark:text-neutral-500 \" data-description></div></div><div class=\"ms-auto\"><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-4 text-gold\" xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" viewBox=\"0 0 16 16\"><path d=\"M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z\"/></svg></span></div></div>",
 						"extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
 						}' class="hidden">
-						<option value="in-progress" selected>Now Hiring</option>
+						
+						<option value="in-progress" <?php if ( ! $status_all ) { echo "selected"; } ?> >Now Hiring</option>
 					</select>
 				</div>
 				<div class="pb-5">
@@ -298,7 +302,7 @@ $arr_job_status = array(
 		</div>
 	</aside>
 
-	<section class="w-full lg:w-2/3 sticky__content md:py-triple xl:py-double 2xl:py-single relative py-0 !pb-0">
+	<section id="jobs-column" class="w-full lg:w-2/3 sticky__content md:py-triple xl:py-double 2xl:py-single relative py-0 !pb-0">
 		<div id="jobs-results">
 			<?php if ($query->have_posts()): ?>
             <?php while ($query->have_posts()): $query->the_post(); ?>
